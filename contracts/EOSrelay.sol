45e2ada7b433347348276767db8aa0953499b22a
Sanghyeon Park <twodude@naver.com>
2018-12-08 03:34:47 +0900

    Update EOSrelay.sol

--- contracts/EOSrelay.sol
+++ contracts/EOSrelay.sol
@@ -1,28 +1,60 @@
 pragma solidity ^0.5.0;
 
 contract EOSrelay {
-    uint256 public genesisBlock;
-    uint256 public highestBlock;
+    /* Block index */
+    uint256 public genesisBlockNumber;
+    uint256 public highestBlockNumber;
   
+    /* Block */
     struct BlockHeader {
-        uint256 previous;   // previous
+        uint256 index;      // block_num
+        bytes32 previous;   // previous
         bytes32 txRoot;     // transaction_mroot
         bytes32 axRoot;     // action_mroot
+        bytes32[]  trxHeaders;
     }
-    
-    mapping(uint256 => BlockHeader) public blocks;
+    mapping(bytes32 => BlockHeader) public blocks;
 
+    /* Transaction */
+    struct Transaction {
+        string  _from;
+        string  _to;
+        uint256 _amount;     // quantity
+    }
+    mapping(bytes32 => Transaction) public trxs;    
+    
     constructor (uint256 blockNumber) public {
-        genesisBlock = blockNumber;
-        highestBlock = blockNumber;
+        genesisBlockNumber = blockNumber;
+        highestBlockNumber = blockNumber;
     }
     
     function resetGenesisBlock(uint256 blockNumber) public {
-        genesisBlock = blockNumber;
-        highestBlock = blockNumber;
+        genesisBlockNumber = blockNumber;
+        highestBlockNumber = blockNumber;
+    }
+
+    function submitBlock(bytes32 blockHash, uint256 index, bytes32 previous, bytes32 txRoot, bytes32 axRoot)
+    public {
+        bytes32[] memory blankTrxHeaders;
+        BlockHeader memory newBlockHeader = BlockHeader(index, previous, txRoot, axRoot, blankTrxHeaders);
+        
+        if (index > highestBlockNumber) {
+            highestBlockNumber = index;
+        }
+        
+        blocks[blockHash] = newBlockHeader;
     }
     
-    function submitBlock(uint256 blockHash, bytes memory context) public {
+    function submitTrx(bytes32 blockHash, bytes32 trxHash, string memory _from, string memory _to, uint256 _amount)
+    public {
+        Transaction memory newTrx = Transaction(_from, _to, _amount);
+        trxs[trxHash] = newTrx;
+        
+        blocks[blockHash].trxHeaders.push(trxHash);
+    }
+    
+    /*
+    function submitBlock(bytes32 blockHash, bytes memory context) public {
         BlockHeader memory header = parseBlockHeader(context);
         uint256 blockNumber = getBlockNumber(context);
         
@@ -44,12 +76,13 @@ contract EOSrelay {
         
         blockNumber = 0;
     }
-
-    function getTxRoot(uint256 blockHash) public view returns (bytes32) {
+    */
+    
+    function getTxRoot(bytes32 blockHash) public view returns (bytes32) {
         return blocks[blockHash].txRoot;
     }
 
-    function getAxRoot(uint256 blockHash) public view returns (bytes32) {
+    function getAxRoot(bytes32 blockHash) public view returns (bytes32) {
         return blocks[blockHash].axRoot;
     }
 }
