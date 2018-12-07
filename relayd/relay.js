d1c214c10d4c16ac286c884fa1b4c6bde1d070be
Keunhak Lim <limkeunhak@gmail.com>
2018-12-08 07:11:10 +0900

    commit

--- relayd/relay.js
+++ relayd/relay.js
@@ -3,49 +3,52 @@ const Web3 = require('web3');
 const fs = require('fs');
 const web3 = new Web3('http://localhost:8545');
 
-module.exports = (options) => {
+module.exports = function (options) {
 	const relayOptions = options;
-	console.log(options);
 	const contract = new web3.eth.Contract(JSON.parse(options.RELAY_ABI), options.RELAY_ADDRESS);
+	const relayAbi = relayOptions.RELAY_ABI;
+	const relayAddress = relayOptions.RELAY_ADDRESS;
+	const chunkSize = relayOptions.CHUNK_SIZE;
+	const eosUrl = relayOptions.EOS_MAINNET_URL;
+	const interval = relayOptions.SLEEP_TIME;
+
+	function getBlockInfo (blockNum) {
+		return axios.post(eosUrl + '/v1/chain/get_block', { 'block_num_or_id':blockNum })
+			.then((result) => { return result.data; }).catch((ex) => { return -1; });
+	}
+
+	function getLastBlockHeight () {
+		// get latest number of block on eth contract
+		return 30000001;
+	}
+
+	function submitBlock (blockHash, index, previous, txRoot, axRoot) {
+		console.log('blockHash: ' + blockHash + ' index: ' + index + ' previous: ' + previous + ' txRoot: ' + txRoot + ' axRoot: ' + axRoot);
+	}
+
+	function submitTrx (blockHash, trxHash, from, to, amount) {
+		console.log('blockHash: ' + blockHash + ' trxHash: ' + trxHash + ' from: ' + from + ' to: ' + to + ' amount: ' + amount);
+	}
+
+	function sleep (ms) {
+		return new Promise (resolve => setTimeout(resolve, ms));
+	}
 
 	return {
-		relayAbi: relayOptions.RELAY_ABI,
-		relayAddress: relayOptions.RELAY_ADDRESS,
-		chunkSize: relayOptions.CHUNK_SIZE,
-		eos_mainnet_url: relayOptions.EOS_MAINNET_URL,
-		interval: relayOptions.SLEEP_TIME,
-		contract: contract,
-		runRelay: async () => {
+		runRelay: async function () {
 			while(1) {
-				let lastBlockHeight = this.getLastBlockHeight();
-				let blockInfo = await this.getBlockInfo(lastBlockHeight);
+				let lastBlockHeight = getLastBlockHeight();
+				let blockInfo = await getBlockInfo(lastBlockHeight);
 				if (blockInfo != -1) {
-					this.summitBlock(blockInfo.id, blockInfo.block_num, blockInfo.previous, blockInfo.transaction_mroot, blockInfo.action_mroot);
+					submitBlock(blockInfo.id, blockInfo.block_num, blockInfo.previous, blockInfo.transaction_mroot, blockInfo.action_mroot);
 					blockInfo.transactions.forEach((element) => {
-						this.summitTrx(blockInfo.id, element.trx.id);
+						submitTrx(blockInfo.id, element.trx.id);
 						console.log(element.trx.transaction);
 					});
 				}
-				await sleep(this.interval);
+				await sleep(interval);
 			}
-		},
-		getBlockInfo: (blockNum) => {
-			return axios.post(EOS_MAINNET_URL + '/v1/chain/get_block', { 'block_num_or_id':blockNum })
-				.then((result) => { return result.data; }).catch((ex) => { return -1; });
-		},
-		getLastBlockHeight: () => {
-			// get latest number of block on eth contract
-			return 30000001;
-		},
-		submitBlock: (blockHash, index, previous, txRoot, axRoot) => {
-			console.log('blockHash: ' + blockHash + ' index: ' + index + ' previous: ' + previous + ' txRoot: ' + txRoot + ' axRoot: ' + axRoot);
-		},
-		submitTrx: (blockHash, trxHash, from, to, amount) => {
-			console.log('blockHash: ' + blockHash + ' trxHash: ' + trxHash + ' from: ' + from + ' to: ' + to + ' amount: ' + amount);
-		},
-		sleep: (ms) => {
-			return new Promise (resolve => setTimeout(resolve, ms));
 		}
-	}
-}
+	};
+};
 
--- relayd/relayd.js
+++ relayd/relayd.js
@@ -1,8 +1,26 @@
 const args = require('yargs').argv;
 const fs = require('fs');
 
+if (args.h) {
+	console.log("Usage: node relayd.js --address=<contract address> --abi=<abi path> [Options]");
+	console.log();
+	console.log("Options");
+	console.log();
+	console.log("\t--interval=<relaying interval>");
+	console.log("\t\t: 컨트랙트를 호출하는 주기를 설정합니다.");
+	console.log("\t--chunksize=<chunk size>");
+	console.log("\t\t: 한번에 읽어오는 블록 개수를 설정합니다.");
+	console.log("\t--eospath=<eos network url>");
+	console.log("\t\t: eos 네트워크 정보를 설정합니다.");
+	console.log("\t--h");
+	console.log("\t\t: 도움말을 출력합니다.");
+	console.log();
+	process.exit(1);
+}
+
 if (!args.address || !args.abi) {
-	console.log('Usage: node relayd.js --address=<contract address> --abi=<abi file>');
+	console.log('Usage: node relayd.js --address=<contract address> --abi=<abi path>');
+	console.log('\tHelp: node relayd.js --h ');
 	process.exit(1);
 }
 
@@ -18,9 +36,9 @@ let relayAbi = fs.readFileSync('../build/EOSrelay.abi').toString();
 const options = {
 	EOS_MAINNET_URL: 'https://api.eosnewyork.io',
 	CHUNK_SIZE: 5,
-	SLEEP_TIME: 1000 * 10,
+	SLEEP_TIME: (args.interval)? Number(args.interval) : 1000 * 100,
 	RELAY_ABI: relayAbi,
-	RELAY_ADDRESS: "0x0a4a4b39bb39b354cc8696757d88cfc856fab488" // args.address
-}
+	RELAY_ADDRESS: '0x0a4a4b39bb39b354cc8696757d88cfc856fab488' // args.address
+};
 
 require('./relay')(options).runRelay();
