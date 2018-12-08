90671dceb07de00b01be659a627d86c9591e9b2a
Keunhak Lim <limkeunhak@gmail.com>
2018-12-08 22:00:58 +0900

    commit

--- relayd/relay.js
+++ relayd/relay.js
@@ -3,32 +3,43 @@ const Web3 = require('web3');
 const fs = require('fs');
 
 module.exports = function (options) {
-	const relayOptions = options;
-	const relayAbi = relayOptions.RELAY_ABI;
-	const relayAddress = relayOptions.RELAY_ADDRESS;
-	const chunkSize = relayOptions.CHUNK_SIZE;
-	const eosUrl = relayOptions.EOS_URL;
-	const interval = relayOptions.SLEEP_TIME;
+	// Relay parameter setting
+	const relayAbi = options.RELAY_ABI;
+	const relayAddress = options.RELAY_ADDRESS;
+	const chunkSize = options.CHUNK_SIZE;
+	const eosUrl = options.EOS_URL;
+	const interval = options.SLEEP_TIME;
 	const web3 = new Web3(options.ETHEREUM_URL);
 	const contract = new web3.eth.Contract(JSON.parse(options.RELAY_ABI), options.RELAY_ADDRESS);
 
-// contract.methods['submitBlock(uint256,bytes)'](123123123, new Buffer("test")).send({from:"0x051977ed8e503d3140e3ae8ecc645b3c9245acc6", gas:2000000}).then(receipt => { console.log(receipt); console.log("!!"); }).catch(ex => { console.log(ex); console.log("??");} );
+	// TODO: options type checkinig
 
 	function getBlockInfo (blockNum) {
 		return axios.post(eosUrl + '/v1/chain/get_block', { 'block_num_or_id':blockNum })
 			.then((result) => { return result.data; }).catch((ex) => { return -1; });
 	}
 
-	function getLastBlockHeight () {
+	async function getLastBlockHeight () {
 		// get latest number of block on eth contract
-		return 30000001;
+		return contract.methods['getHghestBlockNumber()']()
+		 .send({ from: '', gas: options.GAS })
+		 .then(blockHeight => { return blockHeight })
+		 .catch( ex => { console.log(ex); });
 	}
 
 	function submitBlock (blockHash, index, previous, txRoot, axRoot) {
+		contract.methods['submitBlock(bytes32,uint256,bytes32,bytes32,bytes32)'](new Buffer(blockHash), Number(index), new Buffer(previous), new Buffer(txRoot), new Buffer(axRoot))
+		 .send({ from: '', gas: options.GAS })
+		 .then(receipt => { console.log(receipt); })
+		 .catch( ex => { console.log(ex); });
 		console.log('blockHash: ' + blockHash + ' index: ' + index + ' previous: ' + previous + ' txRoot: ' + txRoot + ' axRoot: ' + axRoot);
 	}
 
 	function submitTrx (blockHash, trxHash, from, to, amount) {
+	 	contract.methods['submitTrx(bytes32,bytes32,string,string,uint256)'](new Buffer(blockHash), new Buffer(trxHash), from, to, Number(amount))
+		 .send({ from: '', gas: options.GAS })
+		 .then(receipt => { console.log(receipt); })
+		 .catch( ex => { console.log(ex); });
 		console.log('blockHash: ' + blockHash + ' trxHash: ' + trxHash + ' from: ' + from + ' to: ' + to + ' amount: ' + amount);
 	}
 
@@ -39,7 +50,7 @@ module.exports = function (options) {
 	return {
 		runRelay: async function () {
 			while(1) {
-				let lastBlockHeight = getLastBlockHeight();
+				let lastBlockHeight = await getLastBlockHeight();
 				let blockInfo = await getBlockInfo(lastBlockHeight);
 				if (blockInfo != -1) {
 					submitBlock(blockInfo.id, blockInfo.block_num, blockInfo.previous, blockInfo.transaction_mroot, blockInfo.action_mroot);
